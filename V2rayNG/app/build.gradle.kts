c5d1af8f228bcf0c7df19560aeca7afa2a30270d
hyhy01 <acarbzxcvb@126.com>
2026-03-15 18:36:56 +0800

    .

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.v2ray.ang"
+    namespace = "com.v2.ang"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.v2ray.ang"
+        applicationId = "com.v2.ang"
         minSdk = 24
         targetSdk = 36
         versionCode = 715
